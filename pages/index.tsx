7f3ada862524a17b9ef1103b2fe631c332efb71c
03012009BTC <76449836+03012009BTC@users.noreply.github.com>
2025-05-28 22:25:16 +0200

    Delete pages/index.tsx

--- pages/index.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1>Property Valuator</h1>
-      <p>AI nástroj na odhad trhovej hodnoty nehnuteľností na Slovensku.</p>
-    </div>
-  );
-}
